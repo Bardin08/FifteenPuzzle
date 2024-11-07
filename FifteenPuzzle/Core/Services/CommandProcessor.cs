d9fa81e348f7af460387037dbd3d6065a0f0c6c9
Vladyslav Bardin <vbardin810@gmail.com>
2024-11-07 14:17:29 +0200

    Implement logic to determine if command can be executed now

--- FifteenPuzzle/Core/Services/CommandProcessor.cs
+++ FifteenPuzzle/Core/Services/CommandProcessor.cs
@@ -1,9 +1,27 @@
+using FifteenPuzzle.Cli.Commands;
 using FifteenPuzzle.Core.Commands;
+using FifteenPuzzle.Core.Interfaces;
 
 namespace FifteenPuzzle.Core.Services;
 
-public class CommandProcessor : ICommandProcessor
+public class CommandProcessor(IGameEngine gameEngine) : ICommandProcessor
 {
+    private readonly IGameEngine _gameEngine = gameEngine;
+
+    private static readonly HashSet<string> GeneralCommands =
+    [
+        nameof(HelpCommand),
+        nameof(QuitCommand),
+    ];
+    
+    private static readonly HashSet<string> GameLoopCommands = 
+    [
+        nameof(RestartCommand),
+        nameof(ShowCommand),
+        nameof(MoveTileCommand)
+    ];
+
+
     public bool Execute(IGameCommand command)
     {
         throw new NotImplementedException();
@@ -11,6 +29,22 @@ public class CommandProcessor : ICommandProcessor
 
     public bool CanExecute(IGameCommand command)
     {
-        throw new NotImplementedException();
+        var commandName = command.GetType().Name;
+
+        if (GeneralCommands.Contains(commandName))
+            return true;
+
+        if (_gameEngine.IsRunning)
+        {
+            if (GameLoopCommands.Contains(commandName))
+                return true;
+        }
+        else
+        {
+            if (commandName is nameof(StartCommand))
+                return true;
+        }
+
+        return false;
     }
 }
